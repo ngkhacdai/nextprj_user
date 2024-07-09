7395f436634647153cce8340267daa2361649a5a
ngkhacdai <ngkhacdai@gmail.com>
2024-07-09 11:25:08 +0700

    update socket

--- src/components/productdetail/ShopInfor.js
+++ src/components/productdetail/ShopInfor.js
@@ -6,7 +6,7 @@ import Link from "next/link";
 import { useEffect } from "react";
 import io from "socket.io-client";
 
-const socket = io("http://localhost:3001/", {
+const socket = io("http://localhost:8080/", {
   transports: ["websocket"],
 });
 
